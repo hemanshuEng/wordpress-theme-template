cba2a32d66505783b5ef0bb0562e3a2f20f03e81
Hemanshu Khodiyar <hemanshu.eng@gmail.com>
2020-04-12 12:03:33 +0100

    style and css added

--- theme-name/HemanshuWord/functions.php
+++ theme-name/HemanshuWord/functions.php
@@ -3,4 +3,3 @@ function add_theme_scripts(){
     wp_enqueue_script( 'main', get_template_directory_uri().'/dist/main.js');
 }
 add_action( 'wp_enqueue_scripts', 'add_theme_scripts' );
-echo get_template_directory_uri().'/dist/main.js';
---
+++ theme-name/HemanshuWord/src/js/index.js
@@ -0,0 +1,3 @@
+//import css
+import "../css/main.css"
+console.log("Hello World");
