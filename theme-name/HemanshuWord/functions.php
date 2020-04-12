a19626aa4c04ebb9961b36f24b12b9bcd466dc28
Hemanshu Khodiyar <hemanshu.eng@gmail.com>
2020-04-12 13:31:58 +0100

    cache busting added

--- theme-name/HemanshuWord/functions.php
+++ theme-name/HemanshuWord/functions.php
@@ -1,5 +1,9 @@
 <?php
 function add_theme_scripts(){
-    wp_enqueue_script( 'main', get_template_directory_uri().'/dist/main.js');
+    $fullname = basename(glob(get_template_directory() . "/dist/*js")[0]);
+    wp_enqueue_script( 'main', get_template_directory_uri() . '/dist/' . $fullname, [], null, true);
 }
 add_action( 'wp_enqueue_scripts', 'add_theme_scripts' );
+
+echo get_template_directory_uri();
+echo get_template_directory();
